43ff83d59ed9046ddcfca23e9e3cd1660f75118a
Thành Đạt <54407533+thanhdat24@users.noreply.github.com>
2023-03-20 21:18:47 +0700

    Cai dat trang loi 404

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import router from "./router";
-
-import "./assets/main.css";
 
 const app = createApp(App);
 
