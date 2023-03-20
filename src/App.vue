898f635c86cdc37723c0905853e8f1872a2cf8bf
Thành Đạt <54407533+thanhdat24@users.noreply.github.com>
2023-03-20 14:01:05 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+export default {};
+</script>
+<template>
+  <h1 className="text-red-500 text-3xl font-bold">Hello, Vue.js!</h1>
+</template>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
